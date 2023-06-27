5e88869210d453c572b0b02c157c766b7443add1
Antonio69999 <antoine.chabeauti@gmail.com>
2023-06-27 16:46:34 +0200

    php_part3

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+//Exercice_1
+// $compt = 1;
+
+// while ($compt <= 10) {
+//     echo ($compt);
+//     $compt++;
+// }
+
+//Exercice_2
+
+// $compt = 0;
+// $compt2 = 24;
+
+// while ($compt <= 20) {
+//     $result = ($compt * $compt2);
+//     $compt++;
+//     echo ($result);
+// }
+
+//Exercice_3
+
+// $compt = 100;
+// $compt2 = 24;
+
+// while ($compt >= 10) {
+//     $result = $compt * $compt2;
+//     $compt--;
+//     echo ($result);
+// }
+
+//Exercice 4
+
+// $var = 1;
+
+// while ($var <= 10) {
+//     echo ($var);
+//     $var += ($var / 2);
+// }
+
+//Exercice 5
+
+// for ($steps = 1; $steps <= 15; $steps++)
+// {
+//     echo ("On y arrive presque<br>");
+// }
+
+?>
